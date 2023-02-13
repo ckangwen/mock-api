80ae873783e3e750a1ae49728d962dcff051ec66
ckangwen <2437037469@qq.com>
2023-02-13 17:44:16 +0800

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("mock-api");
   app.use(AuthMiddleware);
+  app.enableCors({
+    origin: "*",
+  });
   await app.listen(3000);
 }
 bootstrap();
