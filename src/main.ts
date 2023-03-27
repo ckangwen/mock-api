451c6390432aed74dc6481fd96b12deae5777870
ckangwen <2437037469@qq.com>
2023-03-27 16:17:26 +0800

    feat: enable cors & change listen port

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from "./app.module";
 import { AuthMiddleware } from "./app.middleware";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix("mock-api");
   app.use(AuthMiddleware);
-  await app.listen(3000);
+  await app.listen(8010);
 }
 bootstrap();
